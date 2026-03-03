500979e6204ddbbfa847a5535ece39fd0fc2202c
ShotCrib77 <Wilmer.Safsten@gmail.com>
2026-03-03 18:13:01 +0100

    game config tweek

--- app/lib/gameConfig.ts
+++ app/lib/gameConfig.ts
@@ -8,5 +8,5 @@ export const PLAYER_VOLUME         = 0.25;
 export const FEEDBACK_TEXTS = [
   `+${(GUESS2_TIME_LIMIT_MS - INITIAL_TIME_LIMIT_MS) / 1000} Sekunder`,
   "Albumbild!",
-  `Refräng och +${(GUESS3_TIME_LIMIT_MS - GUESS3_SEEK_MS - GUESS2_TIME_LIMIT_MS) / 1000} Sekunder`,
+  `+${(GUESS3_TIME_LIMIT_MS - GUESS2_TIME_LIMIT_MS) / 1000} Sekunder`,
 ];
