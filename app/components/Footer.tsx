7f1294a094e14122f189d81466f456401f9c5154
ShotCrib77 <Wilmer.Safsten@gmail.com>
2026-03-03 01:46:16 +0100

    Small fix for footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -9,7 +9,7 @@ export default function Footer() {
                     <Music className="w-4 h-4 text-primary" />
                     <span className="font-heading font-semibold text-foreground">Kentle</span>
                 </div>
-                <p className="flex-1 text-right order-2 lg:order-3">Built with Spotify Web SDK • Not affiliated with Kent</p>
+                <p className="flex-1 order-2 lg:order-3 text-center lg:text-right">Built with Spotify Web SDK • Not affiliated with Kent</p>
             </div>
         </footer>
     );
